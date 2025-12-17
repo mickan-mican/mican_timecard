baf29d9f8ca03445aa00a84294380fe7b20989d1
mickan-mican <207189390+mickan-mican@users.noreply.github.com>
2025-12-18 01:30:39 +0900

    sql修正

--- database.sql
+++ database.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS `mc_player_duty_logs` (
     `citizenid` VARCHAR(50) NOT NULL,
     `job` VARCHAR(50) NOT NULL,
     `duty_duration_seconds` INT(11) DEFAULT 0,
-    `daily_duty_second` INT(11) DEFAULT 0,
+    `daily_duty_seconds` INT(11) DEFAULT 0,
     `last_clock_in_time` BIGINT NULL,
     `duty_history` TEXT,
     PRIMARY KEY (`citizenid`, `job`) 
